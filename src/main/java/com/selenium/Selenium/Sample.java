bd75b54d8a7bf596001d93bcb84a39fcca900773
chandrasekarreddy70950 <83000762+chandrasekarreddy70950@users.noreply.github.com>
2022-02-17 18:03:25 +0530

    Adding master branch first time

---
+++ src/main/java/com/selenium/Selenium/Sample.java
@@ -0,0 +1,15 @@
+package com.selenium.Selenium;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	
+	public void WelcomeTest() {
+		
+		System.out.println("Welcome to selenium");
+	}
+
+
+}
